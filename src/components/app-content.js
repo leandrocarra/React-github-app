5bc36f80663de1de4d625de6ba886e1db3899345
Leandro <leandrocarra@Leandros-MacBook-Air.local>
2018-03-03 22:33:05 -0300

    flow of api

--- src/components/app-content.js
+++ src/components/app-content.js
@@ -1,14 +1,14 @@
 'use strict'
 
-import React, {PropTypes} from 'react'
+import React, { PropTypes } from 'react'
 import Search from './search'
 import UserInfo from './user-info'
 import Actions from './actions'
 import Repos from './repos'
 
-const AppContent = ({userinfo, repos, starred}) => (
+const AppContent = ({userinfo, repos, starred, handleSearch}) => (
   <div className='app'>
-    <Search />
+    <Search handleSearch={handleSearch} />
     {!!userinfo && <UserInfo userinfo={userinfo} /> }
     {!!userinfo && <Actions /> }
     {!!repos.length &&
@@ -35,7 +35,7 @@ const AppContent = ({userinfo, repos, starred}) => (
 )
 
 AppContent.propTypes = {
-  userinfo: PropTypes.object.isRequired,
+  userinfo: PropTypes.object,
   repos: PropTypes.array.isRequired,
   starred: PropTypes.array.isRequired
 }
